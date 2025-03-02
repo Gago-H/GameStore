dffb75edf7f51f866bd3e8950ba1517680da2498
Gago-H <95845046+Gago-H@users.noreply.github.com>
2025-03-01 19:00:49 -0800

    Add DTO, GET and POST methods for retrieving games and adding to existing.

---
+++ DTOs/GameDTO.cs
@@ -0,0 +1,9 @@
+namespace GameStore.DTOs;
+
+public record class GameDTO(
+    int ID, 
+    string Name, 
+    string Genre,
+    decimal Price,
+    DateOnly ReleaseDate
+    );
--- Program.cs
+++ Program.cs
@@ -1,6 +1,53 @@
+using GameStore.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+const string GetGameEndpointName = "GetGame";
+
+List<GameDTO> games = [
+    new (
+        1,
+        "Street Fighter II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2010, 9, 30)),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27))
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.ID == id))
+    .WithName(GetGameEndpointName);
+
+// POST /games
+app.MapPost("games", (CreateGameDTO newGame) => 
+{
+    GameDTO game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.ID}, game);
+});
 
 app.Run();
